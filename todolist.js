7d055311d44f3f64f7cbb501d9099890571d5bb5
Sawyer <zonely.may@gmail.com>
2022-03-09 20:06:04 -0500

    Edit2

--- todolist.js
+++ todolist.js
@@ -1,42 +1,44 @@
 let textInput = document.querySelector(`form input[type="text"]`);
 let timeInput = document.querySelector(`form input[type="date"]`);
-let submitBtn = document.querySelector(`form input[type="submit"]`);
-let taskesDiv = document.querySelector(".taskes");
+
 
 function createTheElement() {
+
     let ele = `
         <div class="task">
             ${textInput.value}
             <li><span>${timeInput.value}</span></li>
             <buttom class="btn">Delete</buttom>
         </div>
-    `
+    `  
     return ele
 }
 
 
+let submitB = document.querySelector(`form input[type="submit"]`);
+let taskesD = document.querySelector(".taskes");
 
-submitBtn.addEventListener("click", (e) => {
+submitB.addEventListener("click", (e) => {
     e.preventDefault();
     // console.log(textInput.value);
 
     if (textInput.value !== "") {
-        taskesDiv.innerHTML += createTheElement();
+        taskesD.innerHTML += createTheElement();
     }
 
     textInput.value = "";
     textInput.focus();
 
-    removeBtn();
+    deleteB();
 
     // theStorage();
 });
 
-function removeBtn() {
+function deleteB() {
     let btn = document.querySelectorAll(".btn");
 
-    btn.forEach((buttom) => {
-        buttom.addEventListener("click", (e) => {
+    btn.forEach((button) => {
+        button.addEventListener("click", (e) => {
             e.preventDefault();
             e.target.parentElement.remove();
         });
